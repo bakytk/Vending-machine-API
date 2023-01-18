4372243e8c6480575d3e940f85ccb962fc131a93
bakyt <bakytkh@outlook.com>
2023-01-18 15:21:17 +0100

    add routes

--- src/server/index.js
+++ src/server/index.js
@@ -2,9 +2,9 @@ const { SERVER_PORT } = process.env;
 import express from "express";
 const app = express();
 
-// var router = require('./routes');
-// app.use('/', router);
+import router from "./routes.js";
+app.use("/", router);
 
 app.listen(SERVER_PORT, async () => {
-  console.log(`Server on`);
+  console.log(`Server running on port: ${SERVER_PORT}`);
 });
--- src/server/routes.js
+++ src/server/routes.js
@@ -1,33 +1,32 @@
-
 const { JWT_SECRET } = process.env;
-const {
-  authenticate,
-  fetch,
-  create,
-  ping,
-  fallback
-} = require('./controllers');
-const { authVerify } = require("./auth");
-const express = require('express');
+import {
+  //authenticate,
+  //fetch,
+  //create,
+  // ping,
+  controllers
+  // fallback
+} from "./controllers.js";
+//const { authVerify } = require("./auth");
+import express from "express";
 const router = express.Router();
-const bodyParser= require('body-parser')
 
-//check cors?
+import bodyParser from "body-parser";
 router.use(bodyParser.json());
 
-if (!JWT_SECRET) {
-  throw new Error("Missing JWT_SECRET env");
-};
-const confirmToken = authVerify(JWT_SECRET);
+// if (!JWT_SECRET) {
+//   throw new Error("Missing JWT_SECRET env");
+// }
+//const confirmToken = authVerify(JWT_SECRET);
 
-router.post("/auth", authenticate);
-router.get("/alive", ping);
-router.get("/movies", confirmToken, fetch);
-router.post("/movies", confirmToken, create);
-router.all("/*", fallback);
+//router.post("/auth", authenticate);
+router.get("/alive", controllers.ping);
+//router.get("/movies", fetch); //confirmToken,
+//router.post("/movies", create);
+router.all("/*", controllers.fallback);
 router.use((error, _, res, __) => {
   console.error(`Processing err: ${error}`);
   return res.status(500).json({ error: "Processing error" });
 });
 
-module.exports = router;
+export default router;
